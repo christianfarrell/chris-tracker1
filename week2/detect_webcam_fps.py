7f6b9f7271a8f6a329e903959a1eb5bdc5802fed
farrellchrist <fotuefarrell@gmail.com>
2026-07-17 17:08:45 -0400

    Translate comments from French to English
    Updated comments in French to English for better clarity.

--- week2/detect_webcam_fps.py
+++ week2/detect_webcam_fps.py
@@ -4,46 +4,46 @@ import cv2
 import time
 from ultralytics import YOLO
 
-# Charge le modèle
+# load the model 
 model = YOLO("yolov8n.pt")
 
-# Ouvre la webcam (0 = intégrée, 1 = externe)
+# Open the webcam  (0 = integrated, 1 = extern)
 cap = cv2.VideoCapture(1)
 
 print("Webcam démarrée — appuie sur Q pour quitter")
 
-# Variables pour calculer le FPS
+# first variable to help calculate the fps 
 prev_time = 0
 
 while True:
-    # Lit une frame de la webcam
+    # read a webcam's frame 
     ret, frame = cap.read()
     if not ret:
         print("Erreur webcam")
         break
 
-    # Calcule le FPS
+    # Calcul  FPS
     curr_time = time.time()
     fps = 1 / (curr_time - prev_time)
     prev_time = curr_time
 
-    # Détection YOLO sur la frame
+    # YOLO's detection on the frame 
     results = model(frame, verbose=False)
 
-    # Dessine les bounding boxes
+    # Draw the boxes' boundaries 
     annotated = results[0].plot()
 
-    # Affiche le FPS sur la fenêtre
+    # show the fps on the window 
     cv2.putText(annotated, f"FPS: {int(fps)}", (10, 30),
                 cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-    # Affiche la fenêtre
+    # show the window 
     cv2.imshow("YOLOv8 - Detection temps réel", annotated)
 
-    # Quitter en appuyant sur Q
+    # Quit with q 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Libère les ressources
+
 cap.release()
 cv2.destroyAllWindows()
