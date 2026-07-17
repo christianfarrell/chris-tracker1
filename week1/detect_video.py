c8ded2792c41ce6e481454a5795e86b468034b44
farrellchrist <fotuefarrell@gmail.com>
2026-07-17 17:02:35 -0400

    Clarify video detection comment in detect_video.py
    Updated comment to clarify video detection usage.

--- week1/detect_video.py
+++ week1/detect_video.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 # Charge le modèle pré-entraîné
 model = YOLO("yolov8n.pt")
 
-# Détection sur une vidéo locale (classes=[0] = personnes seulement)
+# Détect on a local video inside your project file  (classes=[0] = personnes seulement) or you can use C:/Users/nom_user/Videos/myvideo.mp4
 results = model("test_video.mp4", save=True, classes=[0])
 
 print("Vidéo annotée sauvegardée dans runs/detect/")
